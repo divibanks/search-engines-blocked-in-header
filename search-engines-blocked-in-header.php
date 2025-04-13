b09cead523bd92da871791fe27d4ee65ddf2befa
Patrick Lumumba <97313939+lumumbapl@users.noreply.github.com>
2025-04-13 14:53:46 +0300

    Complete refactoring of the plugin
    Added new features

--- search-engines-blocked-in-header.php
+++ search-engines-blocked-in-header.php
@@ -1,37 +1,64 @@
 <?php
 /*
-Plugin Name: Search Engines Blocked in Header
-Plugin URI: https://github.com/divibanks/search-engines-blocked-in-header
-Description: Display the 'Search Engines Discouraged' (or any translation) notification in the WordPress Toolbar if blog_public option has been checked.
-Author: Patrick Lumumba
-Version: 1.1.0
-Author URI: https://lumumbas.blog
-Text Domain: search-engines-blocked-in-header
-Domain Path: /languages/
+* Plugin Name: Search Engines Blocked in Header
+* Plugin URI: https://github.com/divibanks/search-engines-blocked-in-header
+* Description: Display the 'Search Engines Discouraged' notification in the WordPress Toolbar if blog_public option is unchecked.
+* Author: Patrick Lumumba
+* Version: 2.0.0
+* Author URI: https://lumumbas.blog
+* License: GPLv2 or later
+* License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+* Text Domain: search-engines-blocked-in-header
+*
+* @package search-engines-blocked-in-header
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
-add_action( 'admin_bar_menu', 'nostromo_search_engines_blocked', 1000 );
-function nostromo_search_engines_blocked() {
-	global $wp_admin_bar, $wpdb;
+/**
+ * Add notice to admin bar if search engines are discouraged
+ */
+add_action( 'admin_bar_menu', 'sebih_search_engines_blocked', 1000 );
+function sebih_search_engines_blocked() {
+	global $wp_admin_bar;
+
 	if ( ! is_super_admin() || ! is_admin_bar_showing() ) {
 		return;
 	}
-	$url = '/wp-admin/options-reading.php';
+
 	if ( ! get_option( 'blog_public' ) ) {
-		$wp_admin_bar->add_menu( array( 'id'    => 'search_engines_blocked',
-		                                'title' => __( 'Search Engines Discouraged', 'search-engines-blocked-in-header' ),
-		                                'href'  => $url
+		$wp_admin_bar->add_menu( array(
+			'id'    => 'search_engines_blocked',
+			'title' => __( 'Search Engines Discouraged', 'search-engines-blocked-in-header' ),
+			'href'  => admin_url( 'options-reading.php' ),
+			'meta'  => array(
+				'class' => 'sebih-toolbar-alert'
+			)
 		) );
 	}
 }
 
-/* Load Text Domain */
+/**
+ * Load plugin text domain
+ */
 add_action( 'plugins_loaded', 'sebih_load_textdomain' );
 function sebih_load_textdomain() {
 	load_plugin_textdomain( 'search-engines-blocked-in-header', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 }
-?>
+
+/**
+ * Enqueue custom CSS
+ */
+add_action( 'admin_enqueue_scripts', 'sebih_enqueue_assets' );
+function sebih_enqueue_assets() {
+	if ( ! get_option( 'blog_public' ) && is_admin_bar_showing() ) {
+		wp_enqueue_style(
+			'sebih-custom-css',
+			plugin_dir_url( __FILE__ ) . 'css/sebih_custom.css',
+			array(),
+			'1.0.0' // Version added to avoid caching issues
+		);
+	}
+}
